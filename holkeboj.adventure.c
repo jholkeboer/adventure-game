7ba470e80f86ba890e9640e4596611629a4295e8
jholkeboer <jholkeboer@users.noreply.github.com>
2015-07-18 08:46:54 -0700

    created main file

---
+++ holkeboj.adventure.c
@@ -0,0 +1,15 @@
+/*
+Jack Holkeboer
+CS344 Operating Systems
+Instructor: Benjamin Brewster
+July 20, 2015
+holkeboj@onid.oregonstate.edu
+*/
+
+struct room {
+	char *roomName;
+	int roomType;	/* 
+	For start room, roomType = 1
+	For middle room, roomType = 2
+	For end room, roomType = 3*/	
+};
