eac5d556553cb800fa2c803a7385c4193e4480f5
jholkeboer <jholkeboer@users.noreply.github.com>
2015-07-20 14:23:08 -0700

    creating directory

--- holkeboj.adventure.c
+++ holkeboj.adventure.c
@@ -10,6 +10,11 @@ gcc –o holkeboj.adventure holkeboj.adventure.c
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define MAX_LEN 80
 
 typedef enum {START_ROOM, END_ROOM, MID_ROOM} roomPos;
  
@@ -21,7 +26,14 @@ struct room {
 int main(){
 	//	Create 7 room files
 		// in a directory called holkeboj.rooms.<pid>
-	
+	char dir_name[50] = "holkeboj.rooms.";
+	int pid = getpid();
+	char *pidstring;
+	printf("%d\n", pid);
+	printf("%s\n", dir_name);
+	sprintf(pidstring, "%d", pid);
+	strcat(dir_name, pidstring);
+	printf("%s\n", dir_name);
 	// Read room files into memory
 	
 	
@@ -31,7 +43,7 @@ int main(){
 	int stepCount = 0;
 	struct room currentRoom;
 	// SET CURRENT ROOM EQUAL TO START ROOM
-	
+	mkdir(dir_name,S_IRWXU | S_IRWXG | S_IRWXO);
 	
 	while (playing == 1) {
 			//print current room status
@@ -42,9 +54,11 @@ int main(){
 		printf(".\n");
 		printf("WHERE TO? >");
 		
+		//WRITE ROOM NAME TO HISTORY FILE
 		stepCount++;
 		playing = 0;
 	}
 	
+	printf("\nCongratulations! You did it!\n");
 	return 0;
 }
